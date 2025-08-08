f1f843948f38daa840612de5b6eb6b78fc6b00eb
Iwan van der Kleijn <iwanvanderkleijn@gmail.com>
2025-08-08 18:10:05 +0200

    Removed tristore to new repo at: https://github.com/soyrochus/tristore

--- tristore/init_graph.cypher
+++
@@ -1,5 +0,0 @@
-CREATE (n:TestNode {msg: 'Hello, AGE!'})
-  RETURN n;
-
-MATCH (n:TestNode)
-  RETURN n.msg;
--- tristore/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-    
-podman run -d \
-    --name tristore \
-    -e POSTGRES_PASSWORD=secret \
-    -p 5432:5432  \
-    -v db:/var/lib/postgresql/data \
-    -d localhost/tristore-pg
